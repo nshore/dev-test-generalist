2ae2394420d6ca5ffb449cc3495a6057a1cf1b44
Nicholas Shore <nicholas.shore@JohnLewis.co.uk>
2016-11-27 10:48:31 +0000

    Add API call to get Bike by Id using paramter

--- bike-app/src/main/java/com/bikeapp/Database/DatabaseHandler.java
+++ bike-app/src/main/java/com/bikeapp/Database/DatabaseHandler.java
@@ -2,6 +2,7 @@ package com.bikeapp.Database;
 
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
+import com.mongodb.BasicDBObject;
 import com.mongodb.Mongo;
 import com.mongodb.util.JSON;
 
@@ -29,16 +30,30 @@ public class DatabaseHandler {
             return jsonToString;
         //if issues with the above connection and query then return error
         } catch (UnknownHostException e) {
-            // TODO Auto-generated catch block
             return "Error: Could not connect to DB and return All Bikes";
         }
-
     }
 
-//public String getBikeById( int bikeId){
+public String getBikeById(int bikeId){
 //DBCollection collection = mongoClient.getDB(dbName).getCollection("bike");
         //db.getCollection('bike').find({bikeId:2})
-//}
+    //connect to mongoDB and query all data in the bike collection.  Return as String
+    try {
+        mongoClient = new Mongo(host, port);
+        DBCollection collection = mongoClient.getDB(dbName).getCollection("bike");
+
+        BasicDBObject query = new BasicDBObject();
+        query.put("bikeId", bikeId);
+        DBCursor cursor = collection.find(query);
+
+        JSON json = new JSON();
+        String jsonToString = json.serialize(cursor);
+        return jsonToString;
+        //if issues with the above connection and query then return error
+    } catch (UnknownHostException e) {
+        return "Error: Could not query by Bike ID, please ensure it is specified as a parameter '?bikeId=' and is a valid Integer";
+    }
+}
 
 //public String createBike(Int bikeId = 0; String bikeName = ""; String bikeDescription = "") {
 //    }
--- bike-app/src/main/java/com/bikeapp/controller/HomeController.java
+++ bike-app/src/main/java/com/bikeapp/controller/HomeController.java
@@ -3,16 +3,24 @@ package com.bikeapp.controller;
 import com.bikeapp.Database.DatabaseHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
     @RequestMapping("/getAllBikes")
-    public String bikeQueryPage(){
+    public String allBikesAPICall(){
 
         DatabaseHandler bikeQuery = new DatabaseHandler();
         return bikeQuery.getAllBikes();
         //return "this is the bike page";
+    }
+
+    @RequestMapping("/getBikeById")
+    public String bikeByIdAPICall(@RequestParam("bikeId") int bikeId) {
+
+        DatabaseHandler bikeQuery = new DatabaseHandler();
+        return bikeQuery.getBikeById(bikeId);
     }
 
 }
