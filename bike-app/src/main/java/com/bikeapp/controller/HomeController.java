b0ab59ecd76301a974121ff0805014a300f5b970
Nicholas Shore <nicholas.shore@JohnLewis.co.uk>
2016-11-27 13:16:43 +0000

    Use id in path instead of on param to capture bike id

--- bike-app/src/main/java/com/bikeapp/controller/HomeController.java
+++ bike-app/src/main/java/com/bikeapp/controller/HomeController.java
@@ -1,7 +1,8 @@
 package com.bikeapp.controller;
 
-import com.bikeapp.Database.DatabaseHandler;
-import org.springframework.stereotype.Controller;
+import com.bikeapp.Database.DBQueryHandler;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +12,15 @@ public class HomeController {
     @RequestMapping("/getAllBikes")
     public String allBikesAPICall(){
 
-        DatabaseHandler bikeQuery = new DatabaseHandler();
+        DBQueryHandler bikeQuery = new DBQueryHandler();
         return bikeQuery.getAllBikes();
         //return "this is the bike page";
     }
 
-    @RequestMapping("/getBikeById")
-    public String bikeByIdAPICall(@RequestParam("bikeId") int bikeId) {
+    @RequestMapping("/getBikeById/{bikeId}")
+    public String bikeByIdAPICall(@PathVariable("bikeId") int bikeId, HttpServletRequest request) {
 
-        DatabaseHandler bikeQuery = new DatabaseHandler();
+        DBQueryHandler bikeQuery = new DBQueryHandler();
         return bikeQuery.getBikeById(bikeId);
     }
 
