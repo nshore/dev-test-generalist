d839c8c3aa4d7c3b8140b8b9e2f36044f7ec45ed
Nicholas Shore <nicholas.shore@JohnLewis.co.uk>
2016-11-28 20:54:53 +0000

    Add comments to jsonParser class

--- bike-app/src/main/java/com/bikeapp/utils/JsonParser.java
+++ bike-app/src/main/java/com/bikeapp/utils/JsonParser.java
@@ -4,10 +4,13 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class JsonParser {
+
+    //accept json string and return trimmed json object
     public static JSONObject getStrippedJsonObject (String jsonString){
 
         JSONObject trimmedJson = null;
 
+        //remove brackets at start and end if they exist
         jsonString = jsonString.startsWith("[") ? jsonString.substring(1) : jsonString;
         jsonString = jsonString.endsWith("]") ? jsonString.substring(0,jsonString.length() - 1) : jsonString;
 
@@ -20,10 +23,12 @@ public class JsonParser {
         return trimmedJson;
     }
 
+    //accept json string and return trimmed json string
     public static String getStrippedJsonString (String jsonString){
 
         String trimmedJson = "";
 
+        //remove brackets at start and end if they exist
         jsonString = jsonString.startsWith("[") ? jsonString.substring(1) : jsonString;
         trimmedJson = jsonString.endsWith("]") ? jsonString.substring(0,jsonString.length() - 1) : jsonString;
 
